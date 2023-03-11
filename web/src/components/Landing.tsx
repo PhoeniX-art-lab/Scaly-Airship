c8ab23aee820753481e886913ce0342fb0805813
Danila Berazhny <dannmmaster@gmail.com>
2023-03-11 14:39:23 +0300

    SA-46 - style landing page + eslint

--- web/src/components/Landing.tsx
+++ web/src/components/Landing.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import TwitterLogo from "../styles/assets/twitter-logo.png"
+import "../styles/landing.css"
 
 function Landing() {
   return (
